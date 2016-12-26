01d18aa93041f107cd59aa8cdceb5b77230a685d
Zafar Saleem <zafarsaleem3@gmail.com>
2016-12-26 09:38:24 -0500

    Added active state

--- src/scripts/views/DateView.js
+++ src/scripts/views/DateView.js
@@ -11,12 +11,23 @@ const DateView = {
         _model = model;
 
         render();
+
+        $('body').on('click', '.a-date', onDateClickHandler.bind(this));
     }
 };
 
 function render() {
     console.log(_model.get());
     $('.wrapper').html(dateTemplate({ data: _model.get() }));
+}
+
+function onDateClickHandler(e) {
+    let el = $(e.target);
+
+    $('.a-date').removeClass('active');
+    el.addClass('active');
+
+    e.preventDefault();
 }
 
 module.exports = DateView;
---
+++ src/scripts/views/TimeView.js
@@ -0,0 +1,34 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+// import DateTemplate from '../../templates/DateTemplate.html';
+
+let _model, $els;
+// let dateTemplate = _.template(DateTemplate);
+
+const TimeView = {
+    init: (model) => {
+        _model = model;
+
+        render();
+
+        $('body').on('click', '.a-date', onDateClickHandler.bind(this));
+    }
+};
+
+function render() {
+    console.log(_model.get());
+    // $('.wrapper').html(dateTemplate({ data: _model.get() }));
+}
+
+function onDateClickHandler(e) {
+    let el = $(e.target);
+
+    $('.a-date').removeClass('active');
+    el.addClass('active');
+
+    e.preventDefault();
+}
+
+module.exports = TimeView;
+
